9ed37b427d40037cc6b61addc0a0f8e2e4f8f6d5
ROSS CLAYDON <b7013097@hallam.shu.ac.uk>
2018-03-22 13:51:47 +0000

    2nd Commit- Setup Interface Created

--- JukeBox/Form1.Designer.cs
+++ JukeBox/Form1.Designer.cs
@@ -28,32 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JukeBox_frm));
             this.label1 = new System.Windows.Forms.Label();
             this.GenreList_LstBox = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Playing_txtBox = new System.Windows.Forms.TextBox();
             this.PlayList_LstBox = new System.Windows.Forms.ListBox();
-            this.ToolBar_MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.Setup_txtBoxMenu = new System.Windows.Forms.ToolStripTextBox();
             this.About_txtBoxMenu = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.ToolBar_MenuStrip = new System.Windows.Forms.MenuStrip();
             this.ToolBar_MenuStrip.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
-            this.bindingNavigator1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -105,18 +90,12 @@
             this.PlayList_LstBox.Size = new System.Drawing.Size(287, 204);
             this.PlayList_LstBox.TabIndex = 4;
             // 
-            // ToolBar_MenuStrip
+            // hScrollBar1
             // 
-            this.ToolBar_MenuStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ToolBar_MenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.ToolBar_MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.Setup_txtBoxMenu,
-            this.About_txtBoxMenu});
-            this.ToolBar_MenuStrip.Location = new System.Drawing.Point(0, 895);
-            this.ToolBar_MenuStrip.Name = "ToolBar_MenuStrip";
-            this.ToolBar_MenuStrip.Size = new System.Drawing.Size(673, 43);
-            this.ToolBar_MenuStrip.TabIndex = 5;
-            this.ToolBar_MenuStrip.Text = "menuStrip1";
+            this.hScrollBar1.Location = new System.Drawing.Point(148, 388);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(369, 34);
+            this.hScrollBar1.TabIndex = 7;
             // 
             // Setup_txtBoxMenu
             // 
@@ -133,126 +112,18 @@
             this.About_txtBoxMenu.Size = new System.Drawing.Size(100, 39);
             this.About_txtBoxMenu.Text = "About";
             // 
-            // bindingNavigator1
+            // ToolBar_MenuStrip
             // 
-            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
-            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
-            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
-            this.bindingNavigator1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bindingNavigatorMoveFirstItem,
-            this.bindingNavigatorMovePreviousItem,
-            this.bindingNavigatorSeparator,
-            this.bindingNavigatorPositionItem,
-            this.bindingNavigatorCountItem,
-            this.bindingNavigatorSeparator1,
-            this.bindingNavigatorMoveNextItem,
-            this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem});
-            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
-            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
-            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
-            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
-            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
-            this.bindingNavigator1.Name = "bindingNavigator1";
-            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
-            this.bindingNavigator1.Size = new System.Drawing.Size(673, 39);
-            this.bindingNavigator1.TabIndex = 6;
-            this.bindingNavigator1.Text = "bindingNavigator1";
-            // 
-            // bindingNavigatorMoveFirstItem
-            // 
-            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
-            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
-            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorMoveFirstItem.Text = "Move first";
-            // 
-            // bindingNavigatorMovePreviousItem
-            // 
-            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
-            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
-            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
-            // 
-            // bindingNavigatorSeparator
-            // 
-            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 39);
-            // 
-            // bindingNavigatorPositionItem
-            // 
-            this.bindingNavigatorPositionItem.AccessibleName = "Position";
-            this.bindingNavigatorPositionItem.AutoSize = false;
-            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
-            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 39);
-            this.bindingNavigatorPositionItem.Text = "0";
-            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
-            // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(71, 36);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
-            // bindingNavigatorSeparator1
-            // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 39);
-            // 
-            // bindingNavigatorMoveNextItem
-            // 
-            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
-            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
-            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorMoveNextItem.Text = "Move next";
-            // 
-            // bindingNavigatorMoveLastItem
-            // 
-            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
-            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
-            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorMoveLastItem.Text = "Move last";
-            // 
-            // bindingNavigatorSeparator2
-            // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 39);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(36, 36);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
-            // 
-            // hScrollBar1
-            // 
-            this.hScrollBar1.Location = new System.Drawing.Point(148, 388);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(369, 34);
-            this.hScrollBar1.TabIndex = 7;
+            this.ToolBar_MenuStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ToolBar_MenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.ToolBar_MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Setup_txtBoxMenu,
+            this.About_txtBoxMenu});
+            this.ToolBar_MenuStrip.Location = new System.Drawing.Point(0, 895);
+            this.ToolBar_MenuStrip.Name = "ToolBar_MenuStrip";
+            this.ToolBar_MenuStrip.Size = new System.Drawing.Size(673, 43);
+            this.ToolBar_MenuStrip.TabIndex = 5;
+            this.ToolBar_MenuStrip.Text = "menuStrip1";
             // 
             // JukeBox_frm
             // 
@@ -263,7 +134,6 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(673, 938);
             this.Controls.Add(this.hScrollBar1);
-            this.Controls.Add(this.bindingNavigator1);
             this.Controls.Add(this.PlayList_LstBox);
             this.Controls.Add(this.Playing_txtBox);
             this.Controls.Add(this.textBox1);
@@ -276,9 +146,6 @@
             this.Text = "My Juke Box v1.0";
             this.ToolBar_MenuStrip.ResumeLayout(false);
             this.ToolBar_MenuStrip.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
-            this.bindingNavigator1.ResumeLayout(false);
-            this.bindingNavigator1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -291,22 +158,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox Playing_txtBox;
         private System.Windows.Forms.ListBox PlayList_LstBox;
-        private System.Windows.Forms.MenuStrip ToolBar_MenuStrip;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
         private System.Windows.Forms.ToolStripTextBox Setup_txtBoxMenu;
         private System.Windows.Forms.ToolStripTextBox About_txtBoxMenu;
-        private System.Windows.Forms.BindingNavigator bindingNavigator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
-        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
-        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.MenuStrip ToolBar_MenuStrip;
     }
 }
 
---
+++ JukeBox/Form2.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace JukeBox
+{
+    partial class Setup_Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImportTracks_TxtBox = new System.Windows.Forms.TextBox();
+            this.Import_Btn = new System.Windows.Forms.Button();
+            this.ClearImported_Btn = new System.Windows.Forms.Button();
+            this.ImportTracks_GrpBox = new System.Windows.Forms.GroupBox();
+            this.CopyTrack_Btn = new System.Windows.Forms.Button();
+            this.MoveTrack_Btn = new System.Windows.Forms.Button();
+            this.DeleteTrackFromGenre_Btn = new System.Windows.Forms.Button();
+            this.PresentGenre_GrpBox = new System.Windows.Forms.GroupBox();
+            this.PresentGenre_TxtBox = new System.Windows.Forms.TextBox();
+            this.GenreTitle_Lbl = new System.Windows.Forms.Label();
+            this.GenreTitle_Txtbox = new System.Windows.Forms.TextBox();
+            this.Previous_Btn = new System.Windows.Forms.Button();
+            this.Add_Btn = new System.Windows.Forms.Button();
+            this.Delete_Btn = new System.Windows.Forms.Button();
+            this.Next_Btn = new System.Windows.Forms.Button();
+            this.Cancel_Btn = new System.Windows.Forms.Button();
+            this.Ok_Btn = new System.Windows.Forms.Button();
+            this.ImportTracks_GrpBox.SuspendLayout();
+            this.PresentGenre_GrpBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ImportTracks_TxtBox
+            // 
+            this.ImportTracks_TxtBox.Location = new System.Drawing.Point(27, 41);
+            this.ImportTracks_TxtBox.Multiline = true;
+            this.ImportTracks_TxtBox.Name = "ImportTracks_TxtBox";
+            this.ImportTracks_TxtBox.Size = new System.Drawing.Size(394, 579);
+            this.ImportTracks_TxtBox.TabIndex = 1;
+            // 
+            // Import_Btn
+            // 
+            this.Import_Btn.Location = new System.Drawing.Point(27, 642);
+            this.Import_Btn.Name = "Import_Btn";
+            this.Import_Btn.Size = new System.Drawing.Size(394, 53);
+            this.Import_Btn.TabIndex = 2;
+            this.Import_Btn.Text = "Import Tracks From Directory";
+            this.Import_Btn.UseVisualStyleBackColor = true;
+            // 
+            // ClearImported_Btn
+            // 
+            this.ClearImported_Btn.Location = new System.Drawing.Point(27, 733);
+            this.ClearImported_Btn.Name = "ClearImported_Btn";
+            this.ClearImported_Btn.Size = new System.Drawing.Size(394, 53);
+            this.ClearImported_Btn.TabIndex = 3;
+            this.ClearImported_Btn.Text = "Clear Imported Tracks";
+            this.ClearImported_Btn.UseVisualStyleBackColor = true;
+            // 
+            // ImportTracks_GrpBox
+            // 
+            this.ImportTracks_GrpBox.Controls.Add(this.ImportTracks_TxtBox);
+            this.ImportTracks_GrpBox.Controls.Add(this.ClearImported_Btn);
+            this.ImportTracks_GrpBox.Controls.Add(this.Import_Btn);
+            this.ImportTracks_GrpBox.Location = new System.Drawing.Point(12, 12);
+            this.ImportTracks_GrpBox.Name = "ImportTracks_GrpBox";
+            this.ImportTracks_GrpBox.Size = new System.Drawing.Size(450, 820);
+            this.ImportTracks_GrpBox.TabIndex = 4;
+            this.ImportTracks_GrpBox.TabStop = false;
+            this.ImportTracks_GrpBox.Text = "Imported Tracks";
+            // 
+            // CopyTrack_Btn
+            // 
+            this.CopyTrack_Btn.Location = new System.Drawing.Point(488, 95);
+            this.CopyTrack_Btn.Name = "CopyTrack_Btn";
+            this.CopyTrack_Btn.Size = new System.Drawing.Size(241, 59);
+            this.CopyTrack_Btn.TabIndex = 5;
+            this.CopyTrack_Btn.Text = "Copy Track >>";
+            this.CopyTrack_Btn.UseVisualStyleBackColor = true;
+            // 
+            // MoveTrack_Btn
+            // 
+            this.MoveTrack_Btn.Location = new System.Drawing.Point(488, 184);
+            this.MoveTrack_Btn.Name = "MoveTrack_Btn";
+            this.MoveTrack_Btn.Size = new System.Drawing.Size(241, 59);
+            this.MoveTrack_Btn.TabIndex = 6;
+            this.MoveTrack_Btn.Text = "Move Track >>";
+            this.MoveTrack_Btn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteTrackFromGenre_Btn
+            // 
+            this.DeleteTrackFromGenre_Btn.Location = new System.Drawing.Point(488, 354);
+            this.DeleteTrackFromGenre_Btn.Name = "DeleteTrackFromGenre_Btn";
+            this.DeleteTrackFromGenre_Btn.Size = new System.Drawing.Size(241, 229);
+            this.DeleteTrackFromGenre_Btn.TabIndex = 7;
+            this.DeleteTrackFromGenre_Btn.Text = "Delete Track From Genre";
+            this.DeleteTrackFromGenre_Btn.UseVisualStyleBackColor = true;
+            // 
+            // PresentGenre_GrpBox
+            // 
+            this.PresentGenre_GrpBox.Controls.Add(this.Next_Btn);
+            this.PresentGenre_GrpBox.Controls.Add(this.Delete_Btn);
+            this.PresentGenre_GrpBox.Controls.Add(this.Add_Btn);
+            this.PresentGenre_GrpBox.Controls.Add(this.Previous_Btn);
+            this.PresentGenre_GrpBox.Controls.Add(this.GenreTitle_Txtbox);
+            this.PresentGenre_GrpBox.Controls.Add(this.GenreTitle_Lbl);
+            this.PresentGenre_GrpBox.Controls.Add(this.PresentGenre_TxtBox);
+            this.PresentGenre_GrpBox.Location = new System.Drawing.Point(780, 12);
+            this.PresentGenre_GrpBox.Name = "PresentGenre_GrpBox";
+            this.PresentGenre_GrpBox.Size = new System.Drawing.Size(468, 820);
+            this.PresentGenre_GrpBox.TabIndex = 8;
+            this.PresentGenre_GrpBox.TabStop = false;
+            this.PresentGenre_GrpBox.Text = "Present Genre";
+            // 
+            // PresentGenre_TxtBox
+            // 
+            this.PresentGenre_TxtBox.Location = new System.Drawing.Point(31, 140);
+            this.PresentGenre_TxtBox.Multiline = true;
+            this.PresentGenre_TxtBox.Name = "PresentGenre_TxtBox";
+            this.PresentGenre_TxtBox.Size = new System.Drawing.Size(412, 431);
+            this.PresentGenre_TxtBox.TabIndex = 9;
+            // 
+            // GenreTitle_Lbl
+            // 
+            this.GenreTitle_Lbl.AutoSize = true;
+            this.GenreTitle_Lbl.Location = new System.Drawing.Point(26, 41);
+            this.GenreTitle_Lbl.Name = "GenreTitle_Lbl";
+            this.GenreTitle_Lbl.Size = new System.Drawing.Size(118, 25);
+            this.GenreTitle_Lbl.TabIndex = 10;
+            this.GenreTitle_Lbl.Text = "Genre Title";
+            // 
+            // GenreTitle_Txtbox
+            // 
+            this.GenreTitle_Txtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.GenreTitle_Txtbox.ForeColor = System.Drawing.Color.Gray;
+            this.GenreTitle_Txtbox.Location = new System.Drawing.Point(31, 69);
+            this.GenreTitle_Txtbox.Name = "GenreTitle_Txtbox";
+            this.GenreTitle_Txtbox.Size = new System.Drawing.Size(394, 31);
+            this.GenreTitle_Txtbox.TabIndex = 11;
+            this.GenreTitle_Txtbox.Text = "General";
+            // 
+            // Previous_Btn
+            // 
+            this.Previous_Btn.Location = new System.Drawing.Point(31, 619);
+            this.Previous_Btn.Name = "Previous_Btn";
+            this.Previous_Btn.Size = new System.Drawing.Size(134, 150);
+            this.Previous_Btn.TabIndex = 12;
+            this.Previous_Btn.Text = "<<Previous";
+            this.Previous_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Add_Btn
+            // 
+            this.Add_Btn.Location = new System.Drawing.Point(181, 619);
+            this.Add_Btn.Name = "Add_Btn";
+            this.Add_Btn.Size = new System.Drawing.Size(110, 64);
+            this.Add_Btn.TabIndex = 13;
+            this.Add_Btn.Text = "Add";
+            this.Add_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Delete_Btn
+            // 
+            this.Delete_Btn.Location = new System.Drawing.Point(181, 705);
+            this.Delete_Btn.Name = "Delete_Btn";
+            this.Delete_Btn.Size = new System.Drawing.Size(110, 64);
+            this.Delete_Btn.TabIndex = 14;
+            this.Delete_Btn.Text = "Delete";
+            this.Delete_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Next_Btn
+            // 
+            this.Next_Btn.Location = new System.Drawing.Point(309, 619);
+            this.Next_Btn.Name = "Next_Btn";
+            this.Next_Btn.Size = new System.Drawing.Size(134, 150);
+            this.Next_Btn.TabIndex = 13;
+            this.Next_Btn.Text = "Next>>";
+            this.Next_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_Btn
+            // 
+            this.Cancel_Btn.Location = new System.Drawing.Point(1109, 879);
+            this.Cancel_Btn.Name = "Cancel_Btn";
+            this.Cancel_Btn.Size = new System.Drawing.Size(127, 44);
+            this.Cancel_Btn.TabIndex = 9;
+            this.Cancel_Btn.Text = "Cancel";
+            this.Cancel_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Ok_Btn
+            // 
+            this.Ok_Btn.Location = new System.Drawing.Point(944, 879);
+            this.Ok_Btn.Name = "Ok_Btn";
+            this.Ok_Btn.Size = new System.Drawing.Size(127, 44);
+            this.Ok_Btn.TabIndex = 10;
+            this.Ok_Btn.Text = "Ok";
+            this.Ok_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Setup_Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 935);
+            this.Controls.Add(this.Ok_Btn);
+            this.Controls.Add(this.Cancel_Btn);
+            this.Controls.Add(this.PresentGenre_GrpBox);
+            this.Controls.Add(this.DeleteTrackFromGenre_Btn);
+            this.Controls.Add(this.MoveTrack_Btn);
+            this.Controls.Add(this.CopyTrack_Btn);
+            this.Controls.Add(this.ImportTracks_GrpBox);
+            this.Name = "Setup_Frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Setup";
+            this.Load += new System.EventHandler(this.Setup_Frm_Load);
+            this.ImportTracks_GrpBox.ResumeLayout(false);
+            this.ImportTracks_GrpBox.PerformLayout();
+            this.PresentGenre_GrpBox.ResumeLayout(false);
+            this.PresentGenre_GrpBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox ImportTracks_TxtBox;
+        private System.Windows.Forms.Button Import_Btn;
+        private System.Windows.Forms.Button ClearImported_Btn;
+        private System.Windows.Forms.GroupBox ImportTracks_GrpBox;
+        private System.Windows.Forms.Button CopyTrack_Btn;
+        private System.Windows.Forms.Button MoveTrack_Btn;
+        private System.Windows.Forms.Button DeleteTrackFromGenre_Btn;
+        private System.Windows.Forms.GroupBox PresentGenre_GrpBox;
+        private System.Windows.Forms.TextBox PresentGenre_TxtBox;
+        private System.Windows.Forms.TextBox GenreTitle_Txtbox;
+        private System.Windows.Forms.Label GenreTitle_Lbl;
+        private System.Windows.Forms.Button Delete_Btn;
+        private System.Windows.Forms.Button Add_Btn;
+        private System.Windows.Forms.Button Previous_Btn;
+        private System.Windows.Forms.Button Next_Btn;
+        private System.Windows.Forms.Button Cancel_Btn;
+        private System.Windows.Forms.Button Ok_Btn;
+    }
+}
